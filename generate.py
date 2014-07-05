2b1a8b1a38b2b079146f012930580ff8435ff36b
Brandon Amos <bamos@cmu.edu>
2014-07-05 13:27:24 -0700

    Output index.html with PDF and source code for the snippets.

--- generate.py
+++ generate.py
@@ -10,11 +10,13 @@ env = Environment(loader=FileSystemLoader("tmpl"))
 
 snippets_dir = "snippets"
 dist_dir = "dist"
-gen_snippets_dir = dist_dir+"/gen_snippets"
+html_index = "/index.html"
+gen_snippets_dir = "/gen_snippets"
 
 shutil.rmtree(dist_dir, ignore_errors=True)
-os.makedirs(gen_snippets_dir)
+os.makedirs(dist_dir+"/"+gen_snippets_dir)
 
+snippets = []
 for subdir, dirs, files in os.walk(snippets_dir):
   for fname in files:
     trimmedName, ext = os.path.splitext(fname)
@@ -23,13 +25,19 @@ for subdir, dirs, files in os.walk(snippets_dir):
       with open(full_path, "r") as snippet_f:
         gen_tex_name = gen_snippets_dir+"/"+fname
         gen_pdf_name = gen_snippets_dir+"/"+trimmedName+".pdf"
-        with open(gen_tex_name, "w") as f:
+        snippet_content = snippet_f.read()
+        with open(dist_dir+"/"+gen_tex_name, "w") as f:
           f.write(env.get_template("base.jinja.tex").render(
-            content=snippet_f.read()
+            content=snippet_content
           ))
+        snippets.append({
+          'fname': trimmedName,
+          'pdf': gen_pdf_name,
+          'content': snippet_content
+        })
 
-p = Popen(['make', "-f", "../../Makefile.slides", "-C", gen_snippets_dir],
-  stdout=PIPE, stderr=PIPE)
+p = Popen(['make', "-f", "../../Makefile.slides", "-C",
+  dist_dir+"/"+gen_snippets_dir], stdout=PIPE, stderr=PIPE)
 out = p.communicate()
 if out[1]:
   print("Warning: Make stderr non-empty.")
@@ -37,3 +45,8 @@ if out[1]:
   print(out[0].decode())
   print("===Stderr:")
   print(out[1].decode())
+
+with open(dist_dir+"/"+html_index, "w") as idx_f:
+  idx_f.write(env.get_template("index.jinja.html").render(
+    snippets=snippets
+  ))
